fc467df58e357785e4796dbf74f5191bfa98acea
Vivek Radhakrishnan <smallfoot@osiris.cyber.nyu.edu>
2025-09-30 13:19:50 -0400

    Update setup_hw1.sh
    Added check for apt presence and diverted to pacman (for Arch)

--- Linux/setup_hw1.sh
+++ Linux/setup_hw1.sh
@@ -1,17 +1,33 @@
 #!/usr/bin/env bash
-set -ex 
+set -ex
 
-sudo apt-get update
-echo "Installing git..."
-sudo apt install git
+apt_status=$(apt --version)
+if [[ $apt_status == *not found* ]]; then
+  sudo pacman -Su
+  echo "Installing git..."
+  sudo pacman -S git
 
-echo "Installing lcov..."
-sudo apt-get -y install lcov
+  echo "Installing lcov..."
+  sudo pacman -S lcov
 
-echo "Installing AFL++..."
-sudo apt-get -y install afl++
+  echo "Installing AFL++..."
+  sudo pacman -S afl++
 
-echo "Installing GDB..."
-sudo apt-get -y install gdb
+  echp "Installing GDB..."
+  sudo pacman -S gdb
+else
+  sudo apt-get update
+  echo "Installing git..."
+  sudo apt install git
+
+  echo "Installing lcov..."
+  sudo apt-get -y install lcov
+
+  echo "Installing AFL++..."
+  sudo apt-get -y install afl++
+
+  echo "Installing GDB..."
+  sudo apt-get -y install gdb
+fi
 
 echo "Setup for HW1 complete..."
