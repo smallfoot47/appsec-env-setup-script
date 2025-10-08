68977cda04d74176f59fc2988b5bfaa1cb06d479
Vivek Radhakrishnan <smallfoot@osiris.cyber.nyu.edu>
2025-10-08 10:55:00 -0400

    Update setup_hw1.sh
    Added checks for no supported package managers (and fixed a typo)

--- Linux/setup_hw1.sh
+++ Linux/setup_hw1.sh
@@ -3,19 +3,25 @@ set -ex
 
 apt_status=$(apt --version)
 if [[ $apt_status == *not found* ]]; then
-  sudo pacman -Su
-  echo "Installing git..."
-  sudo pacman -S git
+  pacman_status=$(pacman --version)
+  if [[ $pacman_status == *not found* ]]; then # unsupported
+    echo "Package manager not supported! Install apt or pacman"
+    exit 1
+  else  # pacman supported
+    sudo pacman -Su
+    echo "Installing git..."
+    sudo pacman -S git
 
-  echo "Installing lcov..."
-  sudo pacman -S lcov
+    echo "Installing lcov..."
+    sudo pacman -S lcov
 
-  echo "Installing AFL++..."
-  sudo pacman -S afl++
+    echo "Installing AFL++..."
+    sudo pacman -S afl++
 
-  echp "Installing GDB..."
-  sudo pacman -S gdb
-else
+    echo "Installing GDB..."
+    sudo pacman -S gdb
+  fi
+else # apt supported
   sudo apt-get update
   echo "Installing git..."
   sudo apt install git
